6960dead77fb9186957d223a27c8b49868ce999e
simonkatz88 <109811314+simonkatz88@users.noreply.github.com>
2022-11-18 22:37:30 -0600

    Update explorer.js

--- programs/explorer/explorer.js
+++ programs/explorer/explorer.js
@@ -277,13 +277,13 @@ var resolve_address = async function (address) {
 		if (!address.match(/^https?:\/\/web.archive.org\//) && !address.startsWith(window.location.origin)) {
 			// special exemption: show archive but later version
 			if (address.match(/^https?:\/\/(www\.)?(windows93.net)/)) {
-				address = "https://web.archive.org/web/2015-05-05/" + address;
+				address = address;
 			// complete exemptions:
 			} else if (
 				!address.match(/^https?:\/\/(www\.)?(copy.sh|topotech.github.io\/interdimensionalcable|isaiahodhner.io|brie.fi\/ng)/) &&
 				!address.match(/^(file|data|blob):\/\//)
 			) {
-				address = "https://web.archive.org/web/1998/" + address;
+				address = address;
 			}
 		}
 		is_url = true;
